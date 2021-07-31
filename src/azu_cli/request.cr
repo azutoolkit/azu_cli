f628a06c7b25139908c718138a00f23d08346351
Elias Perez <eliasjpr@gmail.com>
2021-07-31 17:40:01 -0400

    Fix request command

--- src/azu_cli/request.cr
+++ src/azu_cli/request.cr
@@ -14,9 +14,9 @@ module AzuCLI
     DESC
 
     option name : String, "--name=Name", "-n Name", "Request name", ""
-    option query : String, "--query=Name:Type", "-q Name:Type", "Request query properties", ""
-    option path : String, "--path=Name:Type", "-p Name:Type", "Request path properties", ""
-    option form : String, "--form=Name:Type", "-f Name:Type", "Request form properties", ""
+    option query : Array(String), "-q Name:Type", "Request query properties" { query << v }
+    option path : Array(String), "-p Name:Type", "Request path properties" { path << v }
+    option form : Array(String), "-f Name:Type", "Request form properties" { form << v }
 
     def run
       template_path = "#{PATH}/#{name}_#{PROGRAM}.cr".downcase
@@ -30,9 +30,6 @@ module AzuCLI
     end
 
     private def template(template_path)
-      query_params = query.split(" ")
-      form_params = form.split(" ")
-
       File.open(template_path, "w") do |file|
         file.puts <<-CONTENT
         # Request Docs https://azutopia.gitbook.io/azu/endpoints/requests
@@ -49,13 +46,13 @@ module AzuCLI
       end
     end
 
-    def render_params(kind)
+    def render_params(kind : Array(String))
       return if kind.empty?
-      params = kind.split(" ")
+
       String.build do |s|
-        s << render_field params
+        s << render_field kind
         s << "\n\t\t"
-        s << render_validate params
+        s << render_validate kind
       end
     end
   end
